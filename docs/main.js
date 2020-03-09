4b352110aac453048ae8ca6e1d5544761fb147bc
MAX5656 <61784585+MAX5656@users.noreply.github.com>
2020-03-09 23:20:36 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
+function setNuberSelectElementContent(elementId, maxValue){
+	let element = document.getElementById(elementId);
+  let text = [...Array(maxValue).keys()].map(s => `<option value="${s}">${s}</option>`).concat();
+  element.insertAdjacentHTML('afterbegin', text);
+}
+/*
 function setNuberSelectElementContent(element, maxValue){
   let text = [...Array(maxValue).keys()].map(s => "<option value='${s}'>${s}</option>").concat();
   element.insertAdjacentHTML('afterbegin', text);
 }
+*/
